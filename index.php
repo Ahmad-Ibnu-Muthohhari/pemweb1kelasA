8fcf1eff6ab0fd14acc31cd7cc807dddd66283c5
Ahmad Ibnu Muthohhari <ahmadibnumuthohhari@saintek.unipdu.ac.id>
2025-11-25 10:24:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "koneksi.php"; ?>
+
+<h2>Aplikasi Data Siswa</h2>
+
+<a href="tambah_siswa.php">+ Tambah Siswa</a> |
+<a href="data_siswa.php">Lihat Data Siswa</a>
+
+<hr>
+
+<p>Selamat datang di aplikasi CRUD sederhana untuk mengelola data siswa.</p>
